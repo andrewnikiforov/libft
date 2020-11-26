e62aed662671fb1bb12df00db81ec8c2f4ef436a
Orbert Millan <omillan@at-l5.msk.21-school.ru>
2020-11-26 14:04:25 +0300

    fix ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: omillan <omillan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 13:04:06 by omillan           #+#    #+#             */
-/*   Updated: 2020/11/08 12:45:11 by omillan          ###   ########.fr       */
+/*   Updated: 2020/11/26 14:02:55 by omillan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
-	size_t	i;
+	int		i;
+	size_t	k;
 
-	i = 0;
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str || !s)
+	if (!s)
 		return (NULL);
-	if (start >= ft_strlen(s))
-		return (str);
-	while (i < len && *(s + start + i))
-	{
+	k = (ft_strlen(s + start) > len) ? len : ft_strlen(s);
+	i = -1;
+	if (!(str = malloc(sizeof(char) * (k + 1))))
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	while (++i < k && *(s + start + i))
 		*(str + i) = *(s + start + i);
-		i++;
-	}
 	*(str + i) = '\0';
 	return (str);
 }
