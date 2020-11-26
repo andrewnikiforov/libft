9f23f799ac1aad93d11c5d26f7dfc11dc710ebe8
Your Name <omillan@at-l5.msk.21-school.ru>
2020-11-26 14:19:19 +0300

    fix ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -21,14 +21,17 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	if (s1)
+		len = ft_strlen(s1);
+	if (s2)
+		len += ft_strlen(s2);
 	i = -1;
 	j = -1;
-	if (!(str = (char *)malloc(len * sizeof(char))))
+	if (!(str = malloc((len + 1) * sizeof(char))))
 		return (NULL);
-	while (++i < len && *(s1 + i))
+	while (++i < (len + 1) && *(s1 + i))
 		*(str + i) = *(s1 + i);
-	while (i + ++j < len - 1 && *(s2 + j))
+	while (i + ++j < len && *(s2 + j))
 		*(str + i + j) = *(s2 + j);
 	*(str + i + j) = '\0';
 	return (str);
