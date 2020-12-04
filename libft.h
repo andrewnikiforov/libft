5eca2df9018c80991fdd4b70afdc70576cec22f9
Andrew Nikiforov <74421277+andrewnikiforov@users.noreply.github.com>
2020-12-04 16:53:24 +0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -68,5 +68,6 @@ t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 					void (*del)(void *));
 int					ft_lstsize(t_list *lst);
+int				get_next_line(int fd, char **line);
 
 #endif
